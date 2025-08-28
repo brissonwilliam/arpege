46776a332ecaf29efbd4d4ca22436b69e9ae47b0
brissonwilliam <brissonwilliam1@hotmail.com>
2025-08-27 22:10:51 -0400

    :sparkles: hello world

---
+++ README.md
@@ -0,0 +1,20 @@
+# Arpege
+
+Arpege is a music library manager that offers modern streaming capabilities like, well, streaming, sharing links and downloading to client devices.
+Bring your own [legally obtained] music. Own your music and stream it!
+
+** motivation **
+
+Arpege exists because as much as I hate iTunes all my music is in there. I can't find any good tool to migrate my iTune library playlists, songs etc. I also like to own my music and it's the only way I can push songs to my iPhone besides being songs. There are currently no other solutions that offers a modern music streaming app, based on an existing library on a filesystem.
+
+### Web ui
+react, htmx? websockets?
+
+### REST server api
+- stream file
+    - Octet streams for files
+    - Transcodes options
+
+### DB
+Sqlite
+
---
+++ server/src/server/server.rs
@@ -0,0 +1,34 @@
+use axum::{routing, Router};
+use log;
+
+#[tokio::main]
+pub async fn start() {
+    log::info!("starting web api");
+
+    let app = Router::new().route("/", routing::get(hello));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8222").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello() -> &'static str {
+    "Hello, World!"
+}
+
+/*
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+*/
