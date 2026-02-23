aadc160d0103234b5c419103a5fe0a13c5ed9ef5
brissonwilliam <brissonwilliam1@hotmail.com>
2026-02-22 20:02:12 -0500

    more ui

--- web/src/Footer.svelte
+++ web/src/Footer.svelte
@@ -1,3 +1,24 @@
-<div class="footer bg-gray-700 block" style="min-width: 60px">
-    <h1>Footer</h1>
+<script>
+    import IconPlay from "@iconify-svelte/iconamoon/play-circle-fill";
+    import IconStart from "@iconify-svelte/iconamoon/player-start-light";
+    import IconEnd from "@iconify-svelte/iconamoon/player-end-light";
+</script>
+
+<div class="footer">
+    <div class="flex justify-between items-center pt-6 mb-2">
+        <div class="w-2/10 text-xs">
+            <p>Now playing (nice icon + song name)</p>
+        </div>
+        <div class="w-8/12">
+            <div class="flex justify-center gap-8">
+                <IconStart class="pt-1" height="2.2rem" />
+                <IconPlay height="2.4rem" />
+                <IconEnd class="pt-1" height="2.2rem" />
+            </div>
+            <div class="mt-3">|----------PROGRESS BAR-------|</div>
+        </div>
+        <div class="w-2/10 text-xs">
+            <p>Volume and shite</p>
+        </div>
+    </div>
 </div>
